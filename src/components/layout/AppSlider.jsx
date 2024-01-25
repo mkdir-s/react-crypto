8270ec3421dae32106b6efe6d4ac9376e2d3cd7d
artem alexeenko <artyom.alexeyenko@gmail.com>
2024-01-25 22:44:59 +0600

    add list for fetched crypto data

--- src/components/layout/AppSlider.jsx
+++ src/components/layout/AppSlider.jsx
@@ -1,16 +1,77 @@
-import { Layout } from 'antd';
+import { Layout, Card, Statistic, List, Typography, Spin } from 'antd';
+import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
+import { useEffect, useState } from 'react';
+import {fakeFetchAssets, fakeFetchCrypto} from '../../api';
 
 const siderStyle = {
-  textAlign: 'center',
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#1677ff',
+  padding: '1rem'
 };
 
+const data = [
+  'Racing car sprays burning fuel into crowd.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+  'Man charged over missing wedding girl.',
+  'Los Angeles battles huge wildfires.',
+];
+
 export default function AppSlider() {
+  const [loading, setLoading] = useState(false);
+  const [crypto, setCrypto] = useState([]);
+  const [assets, setAssets] = useState([]);
+
+  useEffect(() => {
+    async function preload() {
+      setLoading(true);
+      const {result} = await fakeFetchCrypto();
+      const assets = await fakeFetchAssets();
+
+      setAssets(assets);
+      setCrypto(result);
+      setLoading(false);
+    }
+    preload();
+  }, [])
+
+  if (loading) {
+    return <Spin fullscreen />
+  }
+
   return (
         <Layout.Sider width="25%" style={siderStyle}>
-          Sider
+          <Card style={{marginBottom: '1rem'}}>
+            <Statistic
+            title="Active"
+            value={11.28}
+            precision={2}
+            valueStyle={{
+              color: '#3f8600',
+            }}
+            prefix={<ArrowUpOutlined />}
+            suffix="%"
+          />
+          <List
+              size='small'
+              dataSource={data}
+              renderItem={(item) => (
+                <List.Item>
+                  <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                </List.Item>
+              )}
+            />
+          </Card>
+          <Card>
+            <Statistic
+            title="Idle"
+            value={9.3}
+            precision={2}
+            valueStyle={{
+              color: '#cf1322',
+            }}
+            prefix={<ArrowDownOutlined />}
+            suffix="%"
+            />
+          </Card>
         </Layout.Sider>
   )
 }
